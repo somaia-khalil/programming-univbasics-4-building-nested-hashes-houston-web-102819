38cf62e59f894d00d1a157e44c98e1d78104e162
somaia khalil <sumaya_khair@live.com>
2019-10-18 17:01:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,7 +41,7 @@ def monopoly_with_fourth_tier
       :four_pieces_owned => 200 },
    :names => {
      :reading_railroad => {"mortgage_value" => "$100"},
-     :pennsylvania_railroad =>{"mortgage_value" => "$100"},
+     :pennsylvania_railroad =>{"mortgage_value" => "$200"},
      :b_and_o_railroad => {"mortgage_value" => "$100"},
      :shortline_railroad => {"mortgage_value" => "$100"}
      

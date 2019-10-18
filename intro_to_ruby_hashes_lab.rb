c71425cfe9795c600623bc6c14430bc1b641539a
somaia khalil <sumaya_khair@live.com>
2019-10-18 16:12:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  monopoly = {:railroads => {:pieces => 4,:rent_in_dollars => } }
+  monopoly = {:railroads => {:pieces => 4,:rent_in_dollars =>{} } }
 	monopoly
 end
 

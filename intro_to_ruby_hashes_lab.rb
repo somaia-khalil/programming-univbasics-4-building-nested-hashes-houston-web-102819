e545e63bbde7a8a3ae1ac481c03fdb2449fe7664
somaia khalil <sumaya_khair@live.com>
2019-10-18 16:01:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,10 @@
 def base_hash
-	monopoly = {:railroads => {:pieces} }
+	monopoly = {:railroads => {} }
 	monopoly
 end
 
 def monopoly_with_second_tier
-  monopoly = {:railroads => {} }
+  monopoly = {:railroads => {:pieces} }
 	monopoly
 end
 

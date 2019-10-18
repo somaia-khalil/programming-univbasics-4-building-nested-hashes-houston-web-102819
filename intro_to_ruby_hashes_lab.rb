464e1ef803d4f1795db4546f493319ce70401ebf
somaia khalil <sumaya_khair@live.com>
2019-10-18 16:48:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_third_tier
       :one_piece_owned  => 25,
       :two_pieces_owned => 50,
       :three_pieces_owned =>100 ,
-      :me =>4 },
+      :four_pieces_owned => 200 },
    :names => {:name =>1, :mom =>2, :dad =>5 , :me =>4}
   
   }
